cd1ee62aff0abcd074b61e09a36366c0187ed8d6
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2024-09-04 19:43:38 +0530

    my jobs created for recruiter

--- src/components/created-jobs.jsx
+++ src/components/created-jobs.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { getMyJobs } from "@/api/apiJobs";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
+import JobCard from "./job-card";
 
 const CreatedJobs = () => {
-  return (
-    <div>CreatedJobs</div>
-  )
-}
+    const { user } = useUser();
 
-export default CreatedJobs
+    const {
+        loading: loadingCreatedJobs,
+        data: createdJobs,
+        fn: fnCreatedJobs,
+    } = useFetch(getMyJobs, {
+        recruiter_id: user.id,
+    });
+
+    useEffect(() => {
+        fnCreatedJobs();
+    }, []);
+
+    return (
+        <div>
+            {loadingCreatedJobs ? (
+                <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+            ) : (
+                <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {createdJobs?.length ? (
+                        createdJobs.map((job) => {
+                            return (
+                                <JobCard
+                                    key={job.id}
+                                    job={job}
+                                    onJobAction={fnCreatedJobs}
+                                    isMyJob
+                                />
+                            );
+                        })
+                    ) : (
+                        <div>No Jobs Found 😢</div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CreatedJobs;
