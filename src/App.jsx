b658be57006956fe072ae135649d76bfbef29fc2
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2024-09-04 19:33:31 +0530

    my job applications pages created

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import JobListing from "./pages/job-listing"
 import JobPage from "./pages/job"
 import PostJob from "./pages/post-job"
 import SavedJobs from "./pages/saved-job"
-import MyJobs from "./pages/my-job"
+import MyJobs from "./pages/my-jobs"
 import { ThemeProvider } from "./components/theme-provider"
 import ProtectedRoute from "./components/protected-route"
 
--- src/pages/my-job.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyJobs = () => {
-  return (
-    <div>MyJob</div>
-  )
-}
-
-export default MyJobs
